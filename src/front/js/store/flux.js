ec169a79a2980e6dde3b46da203cc01daa3431a1
Juan Jose Bravo Quiroga <juanjo.bravo3@gmail.com>
2025-05-27 02:55:42 +0000

    hola mundo

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://effective-enigma-7v59ppx5prxwfpv5w-3001.app.github.dev/api";
+const BASE_URL = "https://cautious-space-guide-g4rq77pr75qpf9jwp-3001.app.github.dev/api";
 
 
 const getState = ({ getStore, getActions, setStore }) => {
